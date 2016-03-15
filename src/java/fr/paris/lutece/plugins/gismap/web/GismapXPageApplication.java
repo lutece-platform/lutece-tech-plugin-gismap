2f7ce43042ff40d61cc07be95e95859f9f31cdad
basseck <bass.seck@atos.net>
2016-03-15 14:21:59 +0100

    GISMAP-10 : integration of actions on a geo: read, write, delete

--- src/java/fr/paris/lutece/plugins/gismap/web/GismapXPageApplication.java
+++ src/java/fr/paris/lutece/plugins/gismap/web/GismapXPageApplication.java
@@ -4,13 +4,16 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import fr.paris.lutece.plugins.gismap.business.AddressParamHome;
+import fr.paris.lutece.plugins.gismap.service.GismapService;
 import fr.paris.lutece.portal.util.mvc.commons.annotations.Action;
 import fr.paris.lutece.portal.util.mvc.commons.annotations.View;
 import fr.paris.lutece.portal.util.mvc.xpage.MVCApplication;
+import fr.paris.lutece.portal.util.mvc.xpage.annotations.Controller;
 import fr.paris.lutece.portal.web.xpages.XPage;
 
 
-
+@Controller( xpageName = "gismap", pageTitleI18nKey = "gismap.xpage.gismap.pageTitle", pagePathI18nKey = "gismap.xpage.gismap.pagePathLabel" )
 public class GismapXPageApplication extends MVCApplication 
 {
 
@@ -20,7 +23,7 @@ public class GismapXPageApplication extends MVCApplication
 	private static final long serialVersionUID = 1L;
 	
 	// Templates
-    private static final String TEMPLATE_DEFAULT = "/skin/plugins/taxsimulator/create_address.html";
+    private static final String TEMPLATE_DEFAULT = "/skin/plugins/gismap/manage_gismap.html";
 	
 	// Views
 	private static final String VIEW_DEFAULT = "default";
@@ -28,11 +31,18 @@ public class GismapXPageApplication extends MVCApplication
 	// Actions
     private static final String ACTION_CREATE_GEOM = "createGeom";
     
+    // Parameters
+    private static final String PARAMETER_MAP = "map";
+    private static final String PARAMETER_ADD = "add_parameter";
+    
 	
 	@View( value = VIEW_DEFAULT, defaultView = true )
     public XPage getDefaultPage( HttpServletRequest request )
     {
 		Map<String, Object> model = getModel(  );
+        
+        model.put(PARAMETER_ADD, AddressParamHome.getAddressParameters());
+        model.put(PARAMETER_MAP, GismapService.getInstance().getMapTemplate(request));
 		return getXPage( TEMPLATE_DEFAULT, request.getLocale(  ), model );
     }
 	
--- src/sql/plugins/gismap/plugin/create_db_gismap.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS gismap_geometry;
-CREATE TABLE gismap_geometry (		
-id integer,
-geom geometry,
-thematic varchar,
-PRIMARY KEY (id)
-);
