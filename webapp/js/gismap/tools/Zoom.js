fd1d42369a20751fb5c29a982cfce2c2f8ad5feb
pplouy <paul.plouy@atos.net>
2017-12-15 09:31:26 +0100

    GISMAP-199 : ZoomSuggestPoi comes back to previous location if the target is outside view limit extent

--- webapp/js/gismap/tools/Zoom.js
+++ webapp/js/gismap/tools/Zoom.js
@@ -24,6 +24,11 @@ var Zoom = function(GlobalMap, projection, viewGisMap) {
         return current_zoom;
     };
 
+	
+      // An elastic easing method (from https://github.com/DmitryBaranovskiy/raphael).
+      function elastic(t) {
+        return Math.pow(2, -10 * t) * Math.sin((t - 0.075) * (2 * Math.PI) / 0.3) + 1;
+      }
     /**
      * Zoom Method
      * zoomSuggestPoi is a method to call an action to go on a specific area in function of selected elements
@@ -32,12 +37,33 @@ var Zoom = function(GlobalMap, projection, viewGisMap) {
      */
     var zoomSuggestPoi = function (x, y) {
         var pointPoi = new ol.geom.Point([x,y]);
-        viewGisMap.getView().fit(pointPoi, GlobalMap.getSize(),{
-            maxZoom: viewGisMap.getZoomSelect()
-        });
-        if(interact.getEditor()!== null && interact.getEditor().getSuggestPoiEdit()){
-            interact.getEditor().addPoint(pointPoi);
-        }
+		var targetConstrainedCenter = viewGisMap.getView().constrainCenter([x,y]);
+		var previousCenter = viewGisMap.getView().getCenter();
+
+		//if target center is outside the view limit extent, then come-back to the inital location
+		if( targetConstrainedCenter[0] !== x || targetConstrainedCenter[1] !== y){
+			var offsetShakeAnimation = GlobalMap.getView().getResolution()* 100;
+			var shakeCenter = [previousCenter[0] + offsetShakeAnimation, previousCenter[1]];
+			viewGisMap.getView().animate({
+				center: shakeCenter,
+				duration: 250,
+				easing: ol.easing.easeIn
+			},{
+				center: previousCenter,
+				duration: 1500,
+				easing: elastic
+			});	
+			console.log("Zoom error : target address is located outside map boundaries");			
+		}else{
+			viewGisMap.getView().fit(pointPoi,{
+				size: GlobalMap.getSize(),
+				maxZoom: viewGisMap.getZoomSelect(),
+				duration
+			});
+			if(interact.getEditor()!== null && interact.getEditor().getSuggestPoiEdit()){
+				interact.getEditor().addPoint(pointPoi);
+			}
+		}
     };
 
     /**
